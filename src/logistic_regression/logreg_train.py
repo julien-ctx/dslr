9d99a578830dad860093701e735a0174e22d5cb1
julien-ctx <jdaodkql@gmail.com>
2023-04-17 18:21:07 +0200

    add best hand as feature

--- src/logistic_regression/logreg_train.py
+++ src/logistic_regression/logreg_train.py
@@ -1,11 +1,13 @@
 import sys, os
 import pandas as pd
+import numpy as np
 
 class LogisticRegression:
 	@staticmethod
 	def clean_data(df):
 		new_df = df.dropna()
-		new_df['Best Hand'].replace('Left', 0).replace('Right', 1)
+		new_df.loc[new_df['Best Hand'] == 'Left', 'Best Hand'] = 0.0
+		new_df.loc[new_df['Best Hand'] == 'Right', 'Best Hand'] = 1.0
 		return new_df.iloc[:, 5:]
 
 	@staticmethod
@@ -17,8 +19,12 @@ if __name__ == "__main__":
 		sys.exit("Error: wrong parameter number.")
 	if not os.path.exists('../../assets/dataset_train.csv'):
 		sys.exit("Error: dataset doesn't exist.")
-	df = pd.read_csv(sys.argv[1])
+	try:
+		df = pd.read_csv(sys.argv[1])
+	except Exception as e:
+		sys.exit(f"Error: {e}")
 
 	# Drop NaN values, replace Left and Right by boolean values in order to make them count
 	reg_df = LogisticRegression.clean_data(df)
-	
+	reg_df = LogisticRegression.standardize(reg_df)
+	print(reg_df)
