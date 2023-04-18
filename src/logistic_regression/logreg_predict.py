f2829e06e261077f7eb641c4ccbd7fdb0a66b4c6
julien-ctx <jdaodkql@gmail.com>
2023-04-18 17:07:33 +0200

    clean logreg_train

--- src/logistic_regression/logreg_predict.py
+++ src/logistic_regression/logreg_predict.py
@@ -4,5 +4,5 @@ import pandas as pd
 if __name__ == "__main__":
 	if len(sys.argv) != 3:
 		sys.exit("Error: wrong parameter number.")
-	if not os.path.exists('../../assets/dataset_train.csv') or not os.path.exists('weights.csv'):
+	if not os.path.exists(sys.argv[1]) or not os.path.exists(sys.argv[2]):
 		sys.exit("Error: dataset or weights file doesn't exist.")
