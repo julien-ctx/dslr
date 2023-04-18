12a02843cd6a85a2f9294c3de67f7ceb88487dc4
julien-ctx <jdaodkql@gmail.com>
2023-04-18 13:20:26 +0200

    fix problem with describe because of NaN values

--- src/visualization/describe.py
+++ src/visualization/describe.py
@@ -3,22 +3,28 @@ import numpy as np
 import math, sys, os
 
 def mean(x):
-	return np.nansum(x) / x.shape[0]
+	return np.nansum(x) / get_valid_count(x)
 
 def median(x):
-	return x[x.shape[0] // 2] if x.shape[0] % 2 == 1 else (x[x.shape[0] // 2 - 1] + x[x.shape[0] // 2]) / 2
+	valid_count = get_valid_count(x)
+	return x[valid_count // 2] if valid_count % 2 == 1 else (x[valid_count // 2 - 1] + x[valid_count // 2]) / 2
 
 def quartiles(x):
-	return median(x[:x.shape[0] // 2 + (1 if x.shape[0] % 2 == 1 else 0)]), median(x), median(x[x.shape[0] // 2:])
+	valid_count = get_valid_count(x)
+	return median(x[:valid_count // 2 + (1 if valid_count % 2 == 1 else 0)]), median(x), median(x[valid_count // 2:])
 
 def std(x):
+	valid_count = get_valid_count(x)
 	x = (x - mean(x)) ** 2
-	return math.sqrt(np.nansum(x) / x.shape[0])
+	return math.sqrt(np.nansum(x) / valid_count)
+
+def get_valid_count(x):
+	return x.shape[0] - np.sum(pd.isnull(x))
 
 def get_description(a):
-    x = np.sort(a)
-    tmp = quartiles(x)
-    return x.shape[0] - np.sum(pd.isnull(x)), mean(x), std(x), tmp[0], tmp[1], tmp[2], x[0], x[x.shape[0] - 1]
+	x = np.sort(a)
+	tmp = quartiles(x)
+	return get_valid_count(x), mean(x), std(x), tmp[0], tmp[1], tmp[2], x[0], x[-1]
 
 def describe(dataset):
 	return pd.DataFrame(data = np.apply_along_axis(get_description, 0, np.array(dataset)[:,6:]), 
@@ -28,10 +34,13 @@ def describe(dataset):
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
 		sys.exit("Error: wrong parameter number.")
-	if not os.path.exists('../../assets/dataset_train.csv'):
+	if not os.path.exists(sys.argv[1]):
 		sys.exit("Error: dataset doesn't exist.")
 	try:
-		df = pd.read_csv(sys.argv[1])
+		df = pd.read_csv(sys.argv[1]).dropna()
 	except Exception as e:
 		sys.exit(f"Error: {e}")
 	print(describe(df))
+	
+	# Compare with the one of Pandas lib
+	# print(df.iloc[:, 5:].describe())
