9e22b5accd442a619614f755cceaaecdd12be0ae
julien-ctx <jdaodkql@gmail.com>
2023-04-26 17:41:59 +0200

    prediction ok, create house.csv

--- src/logistic_regression/logreg_predict.py
+++ src/logistic_regression/logreg_predict.py
@@ -3,6 +3,16 @@ import pandas as pd
 import numpy as np
 from logreg import LogisticRegressionPredict
 
+def find_max(i, probs):
+	house = -1
+	max_prob = 0.0
+	for j in range(len(probs)):
+		if probs[j][i][0] > max_prob:
+			max_prob = probs[j][i][0]
+			house = j
+	houses = ['Hufflepuff', 'Gryffindor', 'Ravenclaw', 'Slytherin']
+	return houses[house]
+
 if __name__ == "__main__":
 	if len(sys.argv) != 3:
 		sys.exit("Error: wrong parameter number.")
@@ -12,5 +22,13 @@ if __name__ == "__main__":
 	model = LogisticRegressionPredict(sys.argv[1], sys.argv[2])
 
 	houses = ['Hufflepuff', 'Gryffindor', 'Ravenclaw', 'Slytherin']
+	probs = []
+	house_results = []
 	for index, house in enumerate(model.weights_df.columns):
-		print(model.sigmoid(index))
+		probs.append(model.sigmoid(index).tolist())
+	for i in range(model.sample_df.shape[0]):
+		house_results.append(find_max(i, probs))
+	
+	results_df = pd.DataFrame(house_results, columns=['Hogwarts House'])
+	results_df.insert(0, 'Index', results_df.index)
+	results_df.to_csv('../../assets/houses.csv', index=False)
